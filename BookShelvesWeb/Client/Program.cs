bbe57833453b6288cfcb157c748cf01cbdd712cc
derekaz <10210205+derekaz@users.noreply.github.com>
2023-02-27 16:54:33 -0700

    Try this extension to get auth

--- BookShelvesWeb/Client/Program.cs
+++ BookShelvesWeb/Client/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
+using AzureStaticWebApps.Blazor.Authentication;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,8 +9,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
-builder.Services.AddApiAuthorization();
-//builder.Services.AddStaticWebAppsAuthentication();
+//builder.Services.AddApiAuthorization();
+builder.Services.AddStaticWebAppsAuthentication();
 //builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
 
 await builder.Build().RunAsync();
