df72716b7f0e5b1525d058b1d9266c6157aff2c3
derekaz <10210205+derekaz@users.noreply.github.com>
2023-03-05 11:15:32 -0700

    change the DAL to make more generic

--- BookShelvesWeb/Shared/Book.cs
+++ BookShelvesWeb/Shared/Book.cs
@@ -1,16 +1,13 @@
 ﻿using Newtonsoft.Json;
 
-namespace BlazorApp.Shared
+public class Book : IItem
 {
-    public class Book
-    {
-        [JsonProperty(PropertyName = "id")]
-        public string Id { get; set; }
+    [JsonProperty(PropertyName = "id")]
+    public string Id { get; set; }
 
-        [JsonProperty(PropertyName = "title")]
-        public string Title { get; set; }
+    [JsonProperty(PropertyName = "title")]
+    public string Title { get; set; }
 
-        [JsonProperty(PropertyName = "author")]
-        public string Author { get; set; }
-    }
+    [JsonProperty(PropertyName = "author")]
+    public string Author { get; set; }
 }
