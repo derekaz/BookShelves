131c762294275d09999b0c1dc202fa11b0e521a4
Derek Moore <71467714+derekm-aa@users.noreply.github.com>
2023-03-07 14:26:56 -0700

    what a mess...

--- BookShelvesWeb/Client/Program.cs
+++ BookShelvesWeb/Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
 using AzureStaticWebApps.Blazor.Authentication;
+using Blazored.Modal;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 builder.Services.AddStaticWebAppsAuthentication();
+builder.Services.AddBlazoredModal();
 
 await builder.Build().RunAsync();
