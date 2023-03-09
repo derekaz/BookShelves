3a2af22c2905dd0f3a53902dfd4c8778700e16a9
derekaz <10210205+derekaz@users.noreply.github.com>
2023-03-09 10:02:59 -0700

    Cleanup the popup flow and add the title text parameter.

--- BookShelvesWeb/Api/BookFunction/DeleteBook.cs
+++ BookShelvesWeb/Api/BookFunction/DeleteBook.cs
@@ -1,19 +1,11 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using BlazorApp.Api.DataAccess;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using System.Collections;
-using System.Collections.Generic;
-using BlazorApp.Shared;
-using Microsoft.Azure.Cosmos;
-using System.Linq;
-using BlazorApp.Api.DataAccess;
-using Microsoft.AspNetCore.Mvc.Formatters.Internal;
+using System;
+using System.Threading.Tasks;
 
 namespace BlazorApp.Api.BookFunction
 {
