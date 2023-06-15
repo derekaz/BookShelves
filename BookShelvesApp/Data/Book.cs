93cf3f2e1c26c5bf99f5f698729d4b70a700ca01
derekaz <10210205+derekaz@users.noreply.github.com>
2023-06-14 21:39:04 -0700

    Add initial database work

---
+++ BookShelvesApp/Data/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShelves.Data
+{
+    [Table("Books")]
+    internal class Book : BaseTable
+    {
+        public string Title { get; set; }
+
+        public Book() { }
+    }
+}
