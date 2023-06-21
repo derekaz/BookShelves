7e55e7f47ce516e2ba9d647f6ff281e23bf0cbdd
derekaz <10210205+derekaz@users.noreply.github.com>
2023-06-20 20:35:31 -0700

    Update structure to finish content module

---
+++ BookShelvesWeb/Client/Data/BooksDataService.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http.Json;
+using static System.Net.WebRequestMethods;
+
+namespace BlazorApp.Client.Data
+{
+    public class BooksDataService : IBooksDataService
+    {
+        readonly HttpClient _httpClient;
+
+        public BooksDataService(HttpClient http)
+        {
+            _httpClient = http;
+        }
+
+        public async Task<IEnumerable<Book>> GetBooksAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<Book[]>("/api/books") ?? new Book[] { };
+        }
+
+        public async Task<HttpResponseMessage> CreateBookAsync(Book book)
+        {
+            return await _httpClient.PostAsJsonAsync("api/books/new", book);
+        }
+
+        public async Task<HttpResponseMessage> UpdateBookAsync(Book book)
+        {
+            return await _httpClient.PostAsJsonAsync("api/books/edit", book);
+        }
+
+        public async Task<HttpResponseMessage> DeleteBookAsync(Book book)
+        {
+            return await _httpClient.DeleteAsync($"/api/book/{book.Id}");
+        }
+    }
+}
