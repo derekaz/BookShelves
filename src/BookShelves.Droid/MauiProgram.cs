7536730ca6ed0e470b575e78893fa7c4678837e2
derekaz <10210205+derekaz@users.noreply.github.com>
2024-08-03 15:18:03 -0700

    Add Android to Maui Solution

---
+++ src/BookShelves.Droid/MauiProgram.cs
@@ -0,0 +1,17 @@
+﻿using BookShelves.Maui;
+
+namespace BookShelves.Droid
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+
+            builder
+                .UseSharedMauiApp();
+
+            return builder.Build();
+        }
+    }
+}
