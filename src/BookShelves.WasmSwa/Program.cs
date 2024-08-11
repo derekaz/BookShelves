7c89a970dceb30d82dcfa3a0d4669c85139f9898
derekaz <10210205+derekaz@users.noreply.github.com>
2024-08-11 15:53:50 -0700

    lets try this

--- src/BookShelves.WasmSwa/Program.cs
+++ src/BookShelves.WasmSwa/Program.cs
@@ -14,6 +14,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddRazorClassLibraryServices();
 
+builder.Services.AddSingleton<IVersionService, VersionService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddTransient<IBooksDataService, BooksDataService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
