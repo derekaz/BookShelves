7e7250483c0c2bc0e0ecfe2a7f5c6175502c634e
derekaz <10210205+derekaz@users.noreply.github.com>
2024-08-16 19:35:42 -0700

    try this

--- src/BookShelves.Maui/Platforms/MacCatalyst/AppDelegate.cs
+++ src/BookShelves.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -8,7 +8,7 @@ namespace BookShelves.Maui
     {
         protected override MauiApp CreateMauiApp()
         {
-            BookShelves.Maui.Data.SqliteProvider.RegisterSqliteProvider();
+            SqliteProvider.RegisterSqliteProvider();
             return MauiProgram.CreateMauiApp();
         }
     }
