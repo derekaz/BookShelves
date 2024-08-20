24ce0db904be8891c2de3c89c696cedbdfa471cb
derekaz <10210205+derekaz@users.noreply.github.com>
2024-08-19 20:47:55 -0700

    doubt this changes anything

--- src/BookShelves.Maui/Platforms/MacCatalyst/AuthenticationService.cs
+++ src/BookShelves.Maui/Platforms/MacCatalyst/AuthenticationService.cs
@@ -6,7 +6,8 @@ public partial class AuthenticationService
 {
     private partial PublicClientApplicationBuilder AddPlatformConfiguration(PublicClientApplicationBuilder builder)
     {
-        builder.WithRedirectUri("https://login.microsoftonline.com/common/oauth2/nativeclient");
+        builder.WithRedirectUri("http://localhost");
+        //builder.WithRedirectUri("https://login.microsoftonline.com/common/oauth2/nativeclient");
         //builder.WithDefaultRedirectUri();
         builder.WithIosKeychainSecurityGroup("com.microsoft.adalcache");
         return builder;
