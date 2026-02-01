fc99f6cf25e77cdb2b73acd5a28fd5094eb13085
derekaz <10210205+derekaz@users.noreply.github.com>
2026-01-31 20:10:21 -0700

    Attempt to upgrade to net10 - first commit

---
+++ src/BookShelves.Maui.Data/Infrastructure/LocalBookUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using BookShelves.Maui.Data.Models;
+using BookShelves.Maui.Data.Services;
+using BookShelves.Shared.Data.Bases;
+using BookShelves.Shared.Data.Interfaces;
+
+namespace BookShelves.Maui.Data.Infrastructure;
+
+// DataAccess/LocalBookUnitOfWork.cs
+public class LocalBookUnitOfWork(BookShelvesDbContext context) : IUnitOfWork<LocalBook>
+{
+    private readonly BookShelvesDbContext _context = context;
+    private IRepository<LocalBook>? _localBooks;
+
+    public IRepository<LocalBook> LocalBooks => _localBooks ??= new GenericRepository<BookShelvesDbContext, LocalBook>(_context);
+    // Initialize other repositories similarly
+
+    public async Task<int> CompleteAsync() => await _context.SaveChangesAsync();
+
+    public void Dispose() => _context.Dispose();
+}
