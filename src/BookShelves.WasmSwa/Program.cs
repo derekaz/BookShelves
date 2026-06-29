138952ed9b512312d10a987d0792b93dea777827
Derek Moore <derek_m@outlook.com>
2026-06-29 08:15:09 -0700

    Add MudBlazor to WasmSwa project

--- src/BookShelves.WasmSwa/Program.cs
+++ src/BookShelves.WasmSwa/Program.cs
@@ -9,12 +9,15 @@ using BookShelves.Web.Shared.Data;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<WasmApp>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddRazorClassLibraryServices();
+
+builder.Services.AddMudServices();
 
 builder.Services.AddSingleton<IFormFactor, WasmFormFactor>();
 builder.Services.AddSingleton<IVersionService, VersionService>();
