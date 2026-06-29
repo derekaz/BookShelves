8089c1ffec23bbef6964821c88d6fe63e3d6fc42
Derek Moore <derek_m@outlook.com>
2026-06-29 07:57:46 -0700

    Try this one change to get it to build for deployment - then will test deployed app & api

--- src/BookShelves.WasmSwa/Program.cs
+++ src/BookShelves.WasmSwa/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddSingleton<IFormFactor, WasmFormFactor>();
 builder.Services.AddSingleton<IVersionService, VersionService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddTransient<IBook, Book>();
-builder.Services.AddTransient<IBookFactory, BookFactory>();
+builder.Services.AddTransient<IBookFactory, BookViewModelFactory>();
 builder.Services.AddTransient<IBooksDataService, BooksDataService>();
 builder.Services.AddTransient<IBooksSyncService, BooksSyncService>();
 builder.Services.AddTransient<IWeatherForecaster, WasmWeatherForecaster>();
