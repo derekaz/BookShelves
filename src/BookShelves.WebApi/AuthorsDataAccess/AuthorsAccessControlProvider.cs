bfa46721b00d5f97f5b9180c03e5e337cde41aac
Derek Moore <derek_m@outlook.com>
2026-07-14 21:35:20 -0700

    Not sure this will do anything - so frustrating to be able to test

---
+++ src/BookShelves.WebApi/AuthorsDataAccess/AuthorsAccessControlProvider.cs
@@ -0,0 +1,67 @@
+﻿using CommunityToolkit.Datasync.Server;
+using System.Linq.Expressions;
+
+namespace BookShelves.WebApi.AuthorsDataAccess
+{
+    public class AuthorsAccessControlProvider : AccessControlProvider<AuthorItem>
+    {
+        private readonly IHttpContextAccessor _accessor;
+
+        public AuthorsAccessControlProvider(IHttpContextAccessor accessor) { _accessor = accessor; }
+
+        private string? UserId => _accessor.HttpContext?.User?.Identity?.Name;
+
+        public override Expression<Func<AuthorItem, bool>> GetDataView()
+        {
+            return (UserId == null)
+                ? _ => false
+                : _ => true;   // model => model.UserId == UserId;
+        }
+
+        public override ValueTask<bool> IsAuthorizedAsync(TableOperation operation, AuthorItem? entity, CancellationToken cancellationToken = default)
+        {
+            return base.IsAuthorizedAsync(operation, entity, cancellationToken);
+        }
+    }
+}
+
+
+
+//public class PrivateAccessControlProvider<T> : IAccessControlProvider<T>
+//    where T : ITableData
+//    where T : IUserId
+//{
+//    private readonly IHttpContextAccessor _accessor;
+
+//    public PrivateAccessControlProvider(IHttpContextAccessor accessor)
+//    {
+//        _accessor = accessor;
+//    }
+
+//    private string UserId { get => _accessor.HttpContext.User?.Identity?.Name; }
+
+//    public Expression<Func<T, bool>> GetDataView()
+//    {
+//        return (UserId == null)
+//          ? _ => false
+//          : model => model.UserId == UserId;
+//    }
+
+//    public Task<bool> IsAuthorizedAsync(TableOperation op, T entity, CancellationToken token = default)
+//    {
+//        if (op == TableOperation.Create || op == TableOperation.Query)
+//        {
+//            return Task.FromResult(true);
+//        }
+//        else
+//        {
+//            return Task.FromResult(entity?.UserId != null && entity?.UserId == UserId);
+//        }
+//    }
+
+//    public virtual Task PreCommitHookAsync(TableOperation operation, T entity, CancellationToken token = default)
+//    {
+//        entity.UserId == UserId;
+//        return Task.CompletedTask;
+//    }
+//}
--- src/BookShelves.WebApi/Controllers/AuthorItemController.cs
+++ src/BookShelves.WebApi/Controllers/AuthorItemController.cs
@@ -1,14 +1,18 @@
 ﻿using BookShelves.WebApi.AuthorsDataAccess;
 using CommunityToolkit.Datasync.Server;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookShelves.WebApi.Controllers
 {
+    [Authorize]
     [Route("[controller]")]
     public class AuthorItemController : TableController<AuthorItem>
     {
-        public AuthorItemController(IRepository<AuthorItem> repository) : base(repository) 
+        public AuthorItemController(IRepository<AuthorItem> repository, IHttpContextAccessor accessor) : base(repository)
         {
+            AccessControlProvider = new AuthorsAccessControlProvider(accessor);
+
             Options = new TableControllerOptions
             {
                 UnsafeEntityLogging = false
