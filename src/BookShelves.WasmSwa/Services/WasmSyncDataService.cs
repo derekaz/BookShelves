cd164c340fe0ffd1067927389c8e38be6005499a
Derek Moore <71467714+derekm-aa@users.noreply.github.com>
2026-07-22 08:15:10 -0700

    Feature/offlinedata (#78)
    * add a note to link the datasync tools
    
    * First add the nuget dependencies and the first object model
    DbContext is WIP
    
    * basics seems to connect
    
    * Not sure this will do anything - so frustrating to be able to test
    
    * Try to add this one update for now
    
    * Try this commit
    
    * fix the solution path for the gha
    
    * Try this as well
    
    * commit tests to gh so that I can work on them further
    
    * Update the solution
    
    * Start to add datasync client for Maui
    
    * Missed a quick one
    
    * Attempt project update to fix outdated dependency version
    
    * Add UI elements for Author and AuthorViewModel
    
    * WIP Add Data Models
    
    * Try more WIP on integrating the sync context
    
    * More WIP
    
    * The web and webapi seem to store and retrieve data - definitely a WIP though.  Needs a ton of cleanup.
    
    * Fixed the failing tests
    
    * Try these changes
    
    * Fix the build path
    
    * try another change
    
    * Try another way
    
    * oops
    
    * try it closer
    
    * redone another quick change
    
    * try to add a validate step
    
    * Can try this...
    
    * Probably no different
    
    * Can try this but ugh
    
    * Try this config
    
    * Try some more cleanup
    
    * Commit some changes
    
    * Cleanup the web project
    
    * Continued Cleanup
    
    * Try some package version maintenance and remove some unnecessary classes
    
    * Cleanup the Wasm projects to allow to build
    
    ---------
    
    Co-authored-by: Derek Moore <derek_m@outlook.com>
    Co-authored-by: derekaz <10210205+derekaz@users.noreply.github.com>

---
+++ src/BookShelves.WasmSwa/Services/WasmSyncDataService.cs
@@ -0,0 +1,14 @@
+﻿using BookShelves.Shared.Data.Interfaces;
+
+namespace BookShelves.WasmSwa.Services
+{
+    internal class WasmSyncDataService : ISyncDataService
+    {
+        public bool SupportsSync => false;
+
+        public Task ServerSyncAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
