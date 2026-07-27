63ca4ee4dc510a1646ed5217a6a664a8a47f7edc
derekaz <10210205+derekaz@users.noreply.github.com>
2026-07-27 14:58:02 -0700

    Feature/offlinedata (#102)
    * Fix the build path
    
    * try another change
    
    * Try another way
    
    * oops
    
    * try it closer
    
    * redone another quick change
    
    * try to add a validate step
    
    * Can try this...
    
    * Probably no different
    
    * Can try this but ugh
    
    * Try this config
    
    * Try some more cleanup
    
    * Commit some changes
    
    * Cleanup the web project
    
    * Continued Cleanup
    
    * Try some package version maintenance and remove some unnecessary classes
    
    * try another change
    
    * Try another way
    
    * try it closer
    
    * redone another quick change
    
    * try to add a validate step
    
    * Can try this...
    
    * Probably no different
    
    * Try some more cleanup
    
    * Commit some changes
    
    * Continued Cleanup
    
    * Try some package version maintenance and remove some unnecessary classes
    
    * Remove a bad change after the rebase
    
    * One more file fix
    
    * Try to add the ssl - non-matching name error
    
    * try to log whatever error is causing the 500
    
    * Add more error logging
    
    * change log parameter
    
    * Try this for the datasync api connection on the webapi side
    
    * Added some logging for Datasync
    
    * Try to change the logging so that I actually see it
    
    * Added the token to the logging and shortened the response content in the log
    
    * Added logging to the ClientFactory
    
    * Try to add this as well...
    
    * Latest update
    
    * Remove a part of the tableEndpoint value
    
    * Removed the .substring function
    
    * Maybe simplify a bit
    
    * Attempt this change to make it work locally as well
    
    * Update the UI to now have the full services at least in Maui seems to work - albeit slow, it does work.
    
    * One last fix for the web side projects on the adding of the author to the book page
    
    * Cleanup the publish date field
    
    * Cleanup the Authors page from dupe muddialogprovider
    
    * Try some further cleanup to remove some build warnings
    
    * Lets try this with an early build all the way to ios
    
    * Will try this for ios apps so I can see the logs
    
    * Try another fix for CTD on ios
    
    * Try this from copilot
    
    ---------
    
    Co-authored-by: Derek Moore <derek_m@outlook.com>
    Co-authored-by: Derek Moore <71467714+derekm-aa@users.noreply.github.com>

--- src/BookShelves.Maui.Data/Extensions/SqliteProviderExtension.cs
+++ src/BookShelves.Maui.Data/Extensions/SqliteProviderExtension.cs
@@ -3,30 +3,14 @@
 public static class SqliteProviderExtension
 {
     /// <summary>
-    /// Ensures the SQLitePCL provider is initialised for the current platform.
+    /// Intentional no-op.
     /// <para>
-    /// <c>SQLitePCLRaw.bundle_e_sqlite3</c> registers a module initialiser that calls
-    /// <c>raw.SetProvider()</c> automatically when its assembly is first loaded.
-    /// Calling <c>SetProvider()</c> a second time throws
-    /// <see cref="InvalidOperationException"/>. This method is therefore a no-op when
-    /// the bundle has already initialised the provider, which is the normal case.
+    /// This library targets <c>net10.0</c>, so platform symbols such as
+    /// <c>IOS</c>, <c>ANDROID</c>, etc. are never defined here.
+    /// The actual <c>SQLitePCL.raw.SetProvider()</c> call must be made from
+    /// the platform-specific app project where those symbols are defined.
+    /// See <c>MauiProgram.cs</c> for the real initialisation.
     /// </para>
     /// </summary>
-    public static void RegisterSqliteProvider()
-    {
-        try
-        {
-#if WINDOWS || ANDROID
-            SQLitePCL.raw.SetProvider(new SQLitePCL.SQLite3Provider_e_sqlite3());
-#elif IOS || MACCATALYST
-            // Use the system-provided SQLite on iOS/macOS instead of the bundled e_sqlite3
-            // to avoid native-library link issues when the Data project targets net10.0.
-            SQLitePCL.raw.SetProvider(new SQLitePCL.SQLite3Provider_sqlite3());
-#endif
-        }
-        catch (InvalidOperationException)
-        {
-            // Provider was already set by the bundle_e_sqlite3 module initialiser – ignore.
-        }
-    }
+    public static void RegisterSqliteProvider() { }
 }
