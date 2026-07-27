c7c6436702eb6dbde6f264ebda846f03fc1efdaf
derekaz <10210205+derekaz@users.noreply.github.com>
2026-07-27 14:19:01 -0700

    Feature/offlinedata (#101)
    * Fix the build path
    
    * try another change
    
    * Try another way
    
    * oops
    
    * try it closer
    
    * redone another quick change
    
    * try to add a validate step
    
    * Can try this...
    
    * Probably no different
    
    * Can try this but ugh
    
    * Try this config
    
    * Try some more cleanup
    
    * Commit some changes
    
    * Cleanup the web project
    
    * Continued Cleanup
    
    * Try some package version maintenance and remove some unnecessary classes
    
    * try another change
    
    * Try another way
    
    * try it closer
    
    * redone another quick change
    
    * try to add a validate step
    
    * Can try this...
    
    * Probably no different
    
    * Try some more cleanup
    
    * Commit some changes
    
    * Continued Cleanup
    
    * Try some package version maintenance and remove some unnecessary classes
    
    * Remove a bad change after the rebase
    
    * One more file fix
    
    * Try to add the ssl - non-matching name error
    
    * try to log whatever error is causing the 500
    
    * Add more error logging
    
    * change log parameter
    
    * Try this for the datasync api connection on the webapi side
    
    * Added some logging for Datasync
    
    * Try to change the logging so that I actually see it
    
    * Added the token to the logging and shortened the response content in the log
    
    * Added logging to the ClientFactory
    
    * Try to add this as well...
    
    * Latest update
    
    * Remove a part of the tableEndpoint value
    
    * Removed the .substring function
    
    * Maybe simplify a bit
    
    * Attempt this change to make it work locally as well
    
    * Update the UI to now have the full services at least in Maui seems to work - albeit slow, it does work.
    
    * One last fix for the web side projects on the adding of the author to the book page
    
    * Cleanup the publish date field
    
    * Cleanup the Authors page from dupe muddialogprovider
    
    * Try some further cleanup to remove some build warnings
    
    * Lets try this with an early build all the way to ios
    
    * Will try this for ios apps so I can see the logs
    
    * Try another fix for CTD on ios
    
    ---------
    
    Co-authored-by: Derek Moore <derek_m@outlook.com>
    Co-authored-by: Derek Moore <71467714+derekm-aa@users.noreply.github.com>

--- src/BookShelves.Maui/App.xaml.cs
+++ src/BookShelves.Maui/App.xaml.cs
@@ -10,13 +10,29 @@ namespace BookShelves.Maui
         {
             InitializeComponent();
 
-            // Automatically provision the database when the app starts
-            Task.Run(async () =>
-            {
-                using var scope = serviceProvider.CreateScope();
-                var dbContext = scope.ServiceProvider.GetRequiredService<SyncDbContext>();
-                await dbContext.Database.MigrateAsync();
-            });
+            // Automatically provision the database when the app starts.
+                // Exceptions are caught and written to the crash log so that a migration
+                // failure does not become a silent unobserved-task crash on iOS.
+                _ = Task.Run(async () =>
+                {
+                    try
+                    {
+                        using var scope = serviceProvider.CreateScope();
+                        var dbContext = scope.ServiceProvider.GetRequiredService<SyncDbContext>();
+                        await dbContext.Database.MigrateAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        System.Diagnostics.Debug.WriteLine($"[CRITICAL] Database migration failed: {ex}");
+                        try
+                        {
+                            var crashPath = Helpers.FileAccessHelper.GetLogFilePath("db-migration-crash.log");
+                            System.IO.File.AppendAllText(crashPath,
+                                $"=== DB Migration Failure ({DateTime.UtcNow:O}) ===\n{ex}\n\n");
+                        }
+                        catch { /* best-effort only */ }
+                    }
+                });
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
